61cf5dba517a676a12a7326077b79d8f3c62aefd
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-03-15 16:56:43 +0100

    fixed parallax scroll

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// LINKS IN A NEW TAB
+
 const links = document.querySelectorAll("a")
 
 links.forEach(a => a.setAttribute("target", "_blank"))
+
+
+// PARALLAX SCROLL
+
+const parallax = document.querySelectorAll(".parallax");
+
+const parallaxScroll = () => {
+  let offset = window.pageYOffset;
+  parallax.forEach((prllx) => {
+    prllx.style.backgroundPositionY = (offset - prllx.offsetTop) * 0.7 + "px";
+  })
+}
+
+window.addEventListener("scroll", parallaxScroll)
